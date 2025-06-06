12bc08dc85ae3d42c6abf01ba1f7f302b5fa13b9
PetarLakcevic <petar.lakcevic@gmail.com>
2025-06-06 22:01:30 +0200

    week 2

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,157 +1,149 @@
 #%%
-"""
-Task 1
+# Zadatak 1
 
-Write a function that asks the user for a whole number, and prints
-whether the number is even or odd.
-"""
+def odd_or_even():
+    num_str = input("Please enter a whole number\n")
+    num = int(num_str)
+
+    # Option 1
+    # num_eval = 'EVEN' if num % 2 == 0 else 'ODD'
+    # print(f"Number {num} is {num_eval}")
+
+    # Option 2
+    _, reminder = divmod(num, 2)
+    if reminder == 0:
+        print(f"Number {num} is EVEN")
+    else:
+        print(f"Number {num} is ODD")
+
+#%%
+# Test the function
+odd_or_even()
+
+
+#%%
+# Zadatak 2
+
+# f = number * (number -1) * ... * 2 * 1
+def factorial(number):
+    f = 1
+    # Option 1
+    # for i in range(1, number + 1):
+    #     f *= i
+    # return f
+
+    # Option 2
+    for i in range(number, 0, -1):
+        f *= i
+    return f
 
 
 #%%
 # Test the function
-# odd_or_even()
+print(factorial(7))
 
 
 #%%
-"""
-Task 2
+# Zadatak 3
 
-Write a function that calculates and prints the factorial of a number.
-The function accepts the number (should be a positive integer) as its 
-sole input argument. 
-"""
+def nth_lowest(items, n):
+    if n <= 0 or len(items) < n:
+        return min(items)
+    return sorted(items)[n-1]
 
-
-#%%
-# Test the function
-# print(factorial(5))
-
-
-#%%
-"""
-Task 3
-
-Write a function that receives 2 input parameters: 
-1) an iterable (items), and 2) a positive integer (n).  
-The function returns the lowest n-th value of the iterable. 
-If n is not a positive number or it is greater than the number 
-of elements in the iterable, the function returns the lowest 
-value in the iterable.
-"""
 
 
 #%%
 # Test the function with...
 # ... a sequence of numbers:
-# numbers  = [31, 72, 13, 41, 5, 16, 87, 98, 9]
-# print(f"3rd lowest among numbers: {numbers}:")
-# print(nth_lowest(numbers,3))
+numbers  = [31, 72, 13, 41, 5, 16, 87, 98, 9]
+print(f"3rd lowest among numbers: {numbers}:")
+print(nth_lowest(numbers,3))
 
 # ... a sequence of letters:
-# letters = ['f', 'r', 't', 'a', 'b', 'y', 'j', 'd', 'c']
-# print(f"6th lowest among letters: {letters}:")
-# print(nth_lowest(letters, 6))
+letters = ['f', 'r', 't', 'a', 'b', 'y', 'j', 'd', 'c']
+print(f"6th lowest among letters: {letters}:")
+print(nth_lowest(letters, 6))
 
 # ... a string:
-# s = "today"
-# print(f"2nd lowest in string: '{s}':")
-# print(nth_lowest(s, 2))
+s = "today"
+print(f"2nd lowest in string: '{s}':")
+print(nth_lowest(s, 2))
 
 
 #%%
+# Zadatak 4
 
-"""
-Task 4
-
-Write a function that receives a list of numbers and returns
-a tuple with the following elements:
-- the list element with the smallest absolute value
-- the list element with the largest absolute value
-- the sum of all non-negative elements in the list
-- the product of all negative elements in the list
-"""
-
-#%%
-# Test the function
-# print(list_stats([3.4, 5.6, -4.2, -5.6, 9, 1.2, 11.3, -23.45, -81]))
-
-
-#%%
-"""
-Task 5
-
-Write a function that receives a list of numbers and a threshold value. 
-The function:
-- creates a new list with unique elements from the input list
-  that are below the threshold
-- prints the number of elements in the new list
-- sorts the elements in the new list in the descending order,
-  and prints them, one element per line
-"""
-
-#%%
-# Test the function
-# list_operations([1, 1, 2, 3, 5, 8, 13, 5, 21, 34, 55, 89], 20)
-
-
-
-#%%
-"""
-Task 6
-
-Write a function to play a guessing game: to guess a number between 1 and 9.
-Scenario: 
-- User is prompted to guess a number. 
-- If the user guesses wrongly, the prompt reappears
-- The user can try to guess max 3 times
-- On a successful guess, user should get a "Well guessed!" message, and 
-the function terminates. 
-- If when guessing, the user enters a number that is out of the bounds 
-(less than 1 or greater than 9), or a character that is not a number, 
-they should be informed that only single digit values are allowed. This 
-does not count as an unsuccessful guessing attempt.
-
-Hints: 
-- you can use function 'randint' from 'random' package to generate a number to
-be guessed in the game
-- the 'isdigit' string function can be used to check if the user's input is a number 
-"""
+def list_stats(numbers):
+    min_elem = max_elem = numbers[0]
+    sum_non_neg = 0
+    prod_neg = 1
+    for number in numbers:
+        if abs(number) < abs(min_elem):
+            min_elem = number
+        elif abs(number) > abs(max_elem):
+            max_elem = number
+        if number >= 0:
+            sum_non_neg += number
+        else:
+            prod_neg *= number
+    return min_elem, max_elem, sum_non_neg, prod_neg
 
 
 
 #%%
 # Test the function
-# guessing_game()
+print(list_stats([3.4, 5.6, -4.2, -5.6, 9, 1.2, 11.3, -23.45, -81]))
 
 
 #%%
-if __name__ == '__main__':
-    pass
+# Zadatak 5
+def list_operations(numbers, threshold):
+    new_list = []
+    for number in numbers:
+        if number < threshold and number not in new_list:
+            new_list.append(number)
+    print(f"Number of elements in the new list: {len(new_list)}")
+    new_list.sort(reverse=True)
+    for number in new_list:
+        print(number)
 
-    # Task 1
-    # odd_or_even()
+#%%
+# Test the function
+list_operations([1, 1, 2, 3, 5, 8, 13, 5, 21, 34, 55, 89], 20)
 
-    # Task 2
-    # print(factorial(7))
 
-    # Task 3
-    # numbers  = [31, 72, 13, 41, 5, 16, 87, 98, 9]
-    # print(f"3rd lowest among numbers: {numbers}:")
-    # print(nth_lowest(numbers,3))
 
-    # letters = ['f', 'r', 't', 'a', 'b', 'y', 'j', 'd', 'c']
-    # print(f"6th lowest among letters: {letters}:")
-    # print(nth_lowest(letters, 6))
+#%%
+# Zadatak 6
 
-    # s = "today"
-    # print(f"2nd lowest in string: '{s}':")
-    # print(nth_lowest(s, 2))
+def guessing_game():
+    from random import randint
+    number_to_guess = randint(1, 9)
+    print("""
+        Welcome to the Guessing Game!
+        Your task is to guess the number that has been randomly selected among numbers 1 to 9.
+        You may try 3 times. Good luck!
+    """)
+    num_of_attempts = 1
+    while True:
+        num_str = input("Your guess is: ")
+        if not num_str.isdigit() or int(num_str) > 9 or int(num_str) < 1:
+            print("Only digits (1-9) are allowed! Please try again, entering a valid value")
+            continue
+        num = int(num_str)
+        if num == number_to_guess:
+            print(f"Congrats! You have correctly guessed - the number is {num}")
+            return
+        if num_of_attempts < 3:
+            print(f"Wrong! Please try again - you still have {3 - num_of_attempts} attempts")
+            num_of_attempts += 1
+        else:
+            print("Wrong! Sorry, you've made use of all three attempts. More luck next time")
+            return
 
-    # Task 4
-    # print(list_stats([3.4, 5.6, -4.2, -5.6, 9, 1.2, 11.3, -23.45, -81]))
 
-    # Task 5
-    # list_operations([1, 1, 2, 3, 5, 8, 13, 5, 21, 34, 55, 89], 20)
+#%%
+# Test the function
+guessing_game()
 
-    # Task 6
-    # guessing_game()
